6960e5fd0f2d2c8e16f58b2afa8ed38b4301a66e
brittany evans <brittanyevans188@gmail.com>
2024-09-28 09:14:56 +1000

    writing a user model

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Project(models.Model):
@@ -11,7 +12,11 @@ class Project(models.Model):
     image = models.URLField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True)
-
+    owner = models.ForeignKey(
+       get_user_model(),
+       on_delete=models.CASCADE,
+       related_name='owned_projects'
+   )
 class Pledge(models.Model):
    amount = models.IntegerField()
    comment = models.CharField(max_length=200)
@@ -21,3 +26,8 @@ class Pledge(models.Model):
        on_delete=models.CASCADE,
        related_name='pledges'
    ) 
+   supporter = models.ForeignKey(
+       get_user_model(),
+       on_delete=models.CASCADE,
+       related_name='pledges'
+   )
